6c40cb50f25182911dccbf5d428435ce750bee7c
rishii-i <rishiraj.28500@stu.upes.ac.in>
2025-10-08 10:57:40 +0530

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -4,9 +4,12 @@ int main() {
     int sec, hours, minutes;
     printf("Enter time in seconds: ");
     scanf("%d", &sec);
+    
     hours = sec / 3600;
     minutes = (sec % 3600) / 60;
     sec = sec % 60;
+    
     printf("%d : %d : %d \n", hours, minutes, sec);
     return 0;
+
 }
